105857099e7600dbfe68e07bde27ec58e604f671
EbubekirPamuk <58186104+EbubekirPamuk@users.noreply.github.com>
2021-01-19 22:23:54 +0300

    index.html fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import './App.css';
-import {useEffect} from "react";
+import { useEffect } from "react";
 import { io } from 'socket.io-client';
 
 function App() {
 
-      useEffect(() => {
-        const socket = io("http://localhost:3000", {transports:["websocket"],
-      });
-      }, [])
-return <div className="App"></div>
+  useEffect(() => {
+    const socket = io("http://localhost:3000", {
+      transports: ["websocket"],
+    });
+  }, [])
+  
+
+  return <div className="App">
+
+  </div>
+
+
 };
 
 export default App;
