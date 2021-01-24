75aef465faf3b9f7af788f87fab0fb4a7ec05f70
EbubekirPamuk <58186104+EbubekirPamuk@users.noreply.github.com>
2021-01-24 21:37:23 +0300

    fixed index.js in backend folder

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,19 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.end("Hello Socket IO");
+  res.end('Hi Socket.IO');
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  socket.on("new-color", (color) => {
+    console.log(color);
+    
+    socket.broadcast.emit("receive-color", color);
+  });
+
+  socket.on('disconnect', () => console.log("a user disconnected"))
 });
 
 http.listen(3000, () => {
